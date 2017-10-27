dfb1b552d1757c91cefddf878e91d943cb5e722b
Adi Kancherla <findadit@gmail.com>
2017-10-27 08:17:20 -0700

    front end auth

--- public/js/main.js
+++ public/js/main.js
@@ -1,143 +1,55 @@
-$(window).load(function() {
-    $("#preloader").fadeOut("slow");
-});
+function loginFb() {
+  var provider = new firebase.auth.FacebookAuthProvider();
+  provider.addScope('email');
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // The firebase.User instance:
+    var user = result.user;
+    // The Facebook firebase.auth.AuthCredential containing the Facebook
+    // access token:
+    var credential = result.credential;
+  }, function(error) {
+    console.log(JSON.stringify(error));
+  });
+}
 
-$(document).ready(function(){
+function loginGoogle() {
+  // Using a popup.
+  var provider = new firebase.auth.GoogleAuthProvider();
+  provider.addScope('email');
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+  }, function(error) {
+    console.log(JSON.stringify(error));
+  });
+}
 
-    wow = new WOW({
-        mobile:       false,       // default
-      }
-    )
-    wow.init();
+function signUpEmailAndPass(email, password) {
+  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log("email sign up failed with code: " + errorCode + " and message " + errorMessage);
 
-     $('#top-nav').onePageNav({
-        currentClass: 'current',
-        changeHash: true,
-        scrollSpeed: 1200
-    });
+  });
+}
 
-     
-    //animated header class
-    $(window).scroll(function () {
-        if ($(window).scrollTop() > 100) {
-            $(".navbar-default").addClass("animated");
-        } else {
-            $(".navbar-default").removeClass('animated');
-        }
-    });
+function loginEmail(email, password) {
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log("email login failed with code: " + errorCode + " and message " + errorMessage);
 
-    $('#countdown_dashboard').countDown({
-        targetDate: {
-            'day':      11,
-            'month':    3,
-            'year':     2017,
-            'hour':     00,
-            'min':      00,
-            'sec':      01,
-        },
-        omitWeeks: true
-    });
+  });
+}
 
-    $('.init-slider').owlCarousel({
-        items:1,
-        merge:true,
-        loop:true,
-        video:true,
-        smartSpeed: 600
-    });
-
-    /*$('input, textarea').data('holder', $('input, textarea').attr('placeholder'));
-
-    $('input, textarea').focusin(function () {
-        $(this).attr('placeholder', '');
-    });
-    $('input, textarea').focusout(function () {
-        $(this).attr('placeholder', $(this).data('holder'));
-    });*/
-
-
-    //contact form validation
-    $("#contact-form").validate({
-        rules: {
-            name: {
-                required: true,
-                minlength: 2
-            },
-            message: {
-                required: true,
-                minlength: 2
-            },
-            email: {
-                required: true,
-                email: true
-            }
-        },
-        messages: {
-            name: {
-                required: "Please enter Your Name",
-                minlength: "Your name must consist of at least 2 characters"
-            },
-            message: {
-                required: "Please Write Something",
-                minlength: "Your message must consist of at least 2 characters"
-            },
-            email: "Please enter a valid email address"
-        },
-        submitHandler: function(form) {
-            $(form).ajaxSubmit({
-                type:"POST",
-                data: $(form).serialize(),
-                url:"mail.php",
-                success: function() {
-                    $('#contact-form :input').attr('disabled', 'disabled');
-                    $('#contact-form').fadeTo( "slow", 0.15, function() {
-                        $(this).find(':input').attr('disabled', 'disabled');
-                        $(this).find('label').css('cursor','default');
-                        $('#success').fadeIn();
-                    });
-                },
-                error: function() {
-                    $('#contact-form').fadeTo( "slow", 0.15, function() {
-                        $('#error').fadeIn();
-                    });
-                }
-            });
-        }
-    });
-
-});
-
-// When the window has finished loading create our google map below
-google.maps.event.addDomListener(window, 'load', init);
-
-function init() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
-        zoom: 16,
-
-        // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(23.751945, 90.384590), // Dhaka ,
-        scrollwheel: false,
-
-        // How you would like to style the map. 
-        // This is where you would paste any style found on Snazzy Maps.
-        styles: [{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#e0efef"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"hue":"#1900ff"},{"color":"#c0e8e8"}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":100},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"visibility":"on"},{"lightness":700}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#7dcdcd"}]}]
-    };
-
-    // Get the HTML DOM element that will contain your map 
-    // We are using a div with id="map" seen below in the <body>
-    var mapElement = document.getElementById('map-canvas');
-
-    // Create the Google Map using our element and options defined above
-    var map = new google.maps.Map(mapElement, mapOptions);
-
-    // Let's also add a marker while we're at it
-    var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(23.751945, 90.384590),
-        map: map,
-        icon: 'img/map.png',
-        title: 'Twing!'
-    });
+function signOut() {
+  firebase.auth().signOut().then(function() {
+    // Sign-out successful.
+  }).catch(function(error) {
+    // An error happened.
+  });
 }
